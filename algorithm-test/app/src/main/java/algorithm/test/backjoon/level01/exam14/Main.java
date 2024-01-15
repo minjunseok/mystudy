74c1b836d6c2e47aa6615c8ae53378ef9788d013
minjunseok <tjralswnssla@naver.com>
2024-01-15 11:49:41 +0900

    backjoon exam14

---
+++ algorithm-test/app/src/main/java/algorithm/test/backjoon/level01/exam14/Main.java
@@ -0,0 +1,14 @@
+package algorithm.test.backjoon.level01.exam14;
+
+public class Main {
+  public static void main(String[] args) {
+    System.out.print("|\\_/|\n");
+    System.out.print("|q p|   ");
+    System.out.print("/}\n");
+    System.out.print("( 0 )\"\"\"\\\n");
+    System.out.print("|\"^\"`    |\n");
+    System.out.print("||_/=\\\\__|");
+  }
+}
+
+
